bdf1c4a7aa55ff9b7190ac42255c1a731809cf59
Treesaken <mbilalfatiha62@gmail.com>
2025-05-07 21:59:38 +0700

    save lokal

--- Layouts/navbar.php
+++ Layouts/navbar.php
@@ -171,10 +171,7 @@
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-            <?php
-session_start();
-echo $_SESSION['email'] ?? 'Tidak ada session email';
-?>
+            <?= isset($_SESSION['email']) ? $_SESSION['email'] : 'Guest'; ?>
             </span>
             
         </a>
--- root.php
+++ root.php
@@ -1,6 +1,12 @@
 <?php
 require_once('Controllers/Page.php');
 
+session_start();
+if (!isset($_SESSION['email'])) {
+    header("Location: login.php");
+    exit();
+}
+
 if (isset($_GET['url'])) {
     $file = $_GET['url'];
 } else {
